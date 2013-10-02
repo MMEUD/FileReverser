f65af747b5cccc1c5f25b000da5474c61d3b3c9f
Ian Fergus <i.fergus@moodmedia.com>
2013-10-02 09:58:03 +0100

    refactored console Program.cs to captured and display input text of file being reversed ... the Program.cs file remains an example of code smells for refactoring exercise purposes

--- ReverserConsole/Program.cs
+++ ReverserConsole/Program.cs
@@ -19,13 +19,16 @@ namespace ReverserConsole
                 Console.WriteLine("Please enter the location (full path) and filename of the desired ouput file to output reversed text");
                 var outputFile = Console.ReadLine();
 
-                var reversed = CreateReversedOutputFile(inputFile, outputFile);
+                var textFile = CreateTextFile(inputFile);
+                var inputText = textFile.GetFileContents(inputFile);
+                var reversed = CreateReversedOutputFile(textFile, outputFile);
 
                 if (string.IsNullOrEmpty(reversed))
                     Console.WriteLine("File not created.  Exiting ...");
                 else
                 {
                     Console.WriteLine("the input file: " + inputFile);
+                    Console.WriteLine("containing the text:" + inputText);
                     Console.WriteLine("has been reversed and output to the file: " + outputFile);
                     Console.WriteLine("with the reversed content: " + reversed);
 
@@ -40,15 +43,20 @@ namespace ReverserConsole
             }
         }
 
-        private static string CreateReversedOutputFile(string inputFile, string outputFile)
+        private static string CreateReversedOutputFile(IFile textFile, string outputFile)
         {
-            var factory = new FileFactory<IFile>();
-            object[] arguments = {inputFile};
-            IFile textFile = factory.Create<TextFile>(arguments);
             IReverser reverser = new Reverser.Reverser();
             ITextFileReverser textFileReverser = new TextFileReverser(textFile, reverser);
             var reversed = textFileReverser.ReverseTextFileContents(textFile.FileName);
             return textFile.WriteFileContents(outputFile, reversed) ? reversed : string.Empty;
+        }
+
+        private static IFile CreateTextFile(string inputFile)
+        {
+            var factory = new FileFactory<IFile>();
+            object[] arguments = {inputFile};
+            var textFile = factory.Create<TextFile>(arguments);
+            return textFile;
         }
     }
 }
